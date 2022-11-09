328798520960698cdffaa5fbb817ffb3e57cb963
2etimds <2etimds@bglab.local>
2022-11-09 14:12:09 -0300

    Projeto finalizado

---
+++ ds_mvc_asp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ds_mvc_asp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ds_mvc_asp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Usuario()
+        {
+            var usuario = new Usuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+
+        public ActionResult Usuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", usuario);
+            }
+            return View(usuario);
+        }
+
+        public ActionResult Resultado (Usuario usuario)
+        {
+            return View(usuario);
+        }
+        
+    }
+}
